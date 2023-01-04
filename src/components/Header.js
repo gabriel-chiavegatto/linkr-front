ca7aa1a95eda96faa671bed1a72f16c13dbfbdff
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2023-01-03 22:07:42 -0300

    feat: logout menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,33 +2,41 @@ import picture from '../assets/lula.jpg';
 import styled from 'styled-components';
 import arrow from '../assets/arrow.png'
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Header() {
 
     const navigate = useNavigate();
+    const [logOutBar, setLogoutBar] = useState('none');
+    const [arrowDirection, setArrowDirection] = useState('rotate(0deg)')
+
+    function toggleLogoutBar() {
+        if (logOutBar === 'none') {
+            console.log('try')
+            setLogoutBar('block');
+            setArrowDirection('rotate(180deg)')
+        } else {
+            setLogoutBar('none');
+            setArrowDirection('rotate(0deg)')
+        }
+    }
 
     return (
         <>
-            <Head>
+            <Head arrowDirection={arrowDirection}>
                 <h1>LINKR</h1>
                 <section>search</section>
-                <section className='profile'>
-                    <img className='arrow' src={arrow} alt='people' onClick={LogOutBar} />
+                <section className='profile' onClick={toggleLogoutBar} >
+                    <img className='arrow' src={arrow} alt='people' />
                     <img className='user-picture' src={picture} alt='people' />
                 </section>
+                <LogoutAside logOutBar={logOutBar}><p onClick={() => navigate('/')} >Logout</p></LogoutAside>
             </Head>
         </>
 
     )
 }
-function LogOutBar() {
-    console.log("TRY")
-    return (
-        <>
-            <logOutSection>LOGOUT</logOutSection >
-        </>
-    )
-}
+
 
 const Head = styled.div`
 
@@ -40,7 +48,6 @@ const Head = styled.div`
     align-items: center;
     width: 100%;
     height: 72px;
-
     h1{
         padding: 0 28px;
     }
@@ -52,7 +59,7 @@ const Head = styled.div`
     }
     .arrow{
         width: 18.37px;
-        transform: rotate(270deg);
+        transform: ${props => props.arrowDirection};
     }
     .user-picture{
         margin: 0 17px;
@@ -62,16 +69,22 @@ const Head = styled.div`
     }
 `;
 
-const logOutSection = styled.section`
-    position: static;
-    bottom: 0;
+const LogoutAside = styled.aside`
+    display: ${props => props.logOutBar};
+    position: absolute;
+    top: 72px;
     right: 0;
     width: 133px;
     height: 47px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-align: center;
-    background-color: brown;
-    z-index: 10;
+    background: #171717;
+    border-radius: 0px 0px 0px 20px;
+    p{
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+    }
+    
 `;
