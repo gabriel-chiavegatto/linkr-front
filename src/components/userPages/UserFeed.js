7dc54f1521194614a645f262d7722e9bdaf9f19f
Gabriel Chiavegatto <chiavegattogomes@gmail.com>
2023-01-11 18:11:53 -0300

    feat: merge main

--- src/components/userPages/UserFeed.js
+++ src/components/userPages/UserFeed.js
@@ -27,7 +27,7 @@ export function UserFeed() {
         <Container>
             <Main>
                 <TitleBar>
-                    <Title>User's posts</Title>
+                    <Title>{user}'s posts</Title>
                     <button>Follow</button>
                 </TitleBar>
                 <Feed>
